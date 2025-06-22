3324ed1a35901cefa42f4a6909046f3e98e01bc8
Bustah <psnatb2lme@gmail.com>
2025-06-22 21:09:00 +0800

    initial Admin page and buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import RegistrationForm from "./Components/RegistrationForm";
 import ScheduleReceipt from "./Components/ScheduleReceipt";
 import ScheduleSelection from "./Components/Pages/ScheduleSelection";
+import AdminPage from "./Components/AdminPage";
+
 
 export default function App()
 {
   return (
-    <ScheduleSelection />
+    <AdminPage />
   )
 }
