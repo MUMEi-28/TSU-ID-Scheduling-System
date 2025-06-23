c3265d98834b90342cedce58ec53291d9035bcbe
Bustah <psnatb2lme@gmail.com>
2025-06-23 21:26:28 +0800

    New scheduler design complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,17 @@ import ScheduleReceipt from "./Components/Student/Pages/ScheduleReceipt";
 import ScheduleSelection from "./Components/Student/Pages/ScheduleSelection";
 import AdminPage from "./Components/Admin/AdminPage";
 import { Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
+=======
+import QueryPanel from "./Components/Student/NewScheduleSelection/QueryPanel";
+import TimePicker from "./Components/Student/NewScheduleSelection/TimePicker";
+import NewScheduleSelection from "./Components/Student/Pages/NewScheduleSelection";
+>>>>>>> Stashed changes
 
 export default function App()
 {
   return (
+<<<<<<< Updated upstream
     <Routes>
       <Route path='/' element={<RegistrationForm />} />
       <Route path='/schedule/' element={<ScheduleSelection />} />
@@ -14,5 +21,15 @@ export default function App()
       <Route path='/schedule/:date/:timePeriod' element={<ScheduleSelection />} />
       <Route path="/receipt" element={<ScheduleReceipt />} />
     </Routes>
+=======
+    // <Routes>
+    //   <Route path='/' element={<RegistrationForm />} />
+    //   <Route path='/schedule/' element={<ScheduleSelection />} />
+    //   <Route path='/schedule/:date' element={<ScheduleSelection />} />
+    //   <Route path='/schedule/:date/:timePeriod' element={<ScheduleSelection />} />
+    //   <Route path="/receipt" element={<ScheduleReceipt />} />
+    // </Routes>
+    <NewScheduleSelection/>
+>>>>>>> Stashed changes
   )
 }
