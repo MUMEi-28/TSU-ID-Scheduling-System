e80c2830411c0c4c6f7092e96086b2723af49d9a
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-06-25 10:21:58 +0800

    Refactor registration and schedule selection UI
    Removed NewRegistrationForm and merged its design into RegistrationForm for a unified student access experience. Improved code formatting and structure in NewScheduleSelection, TimePicker, and ScheduleReceipt components. Updated TimePicker to use a Link for navigation after scheduling. Changed the app title in index.html to 'TSU ID Schedule'.

--- frontend/src/Components/Student/Pages/NewScheduleSelection.jsx
+++ frontend/src/Components/Student/Pages/NewScheduleSelection.jsx
@@ -4,18 +4,19 @@ import QueryPanel from '../NewScheduleSelection/QueryPanel'
 import TimePicker from '../NewScheduleSelection/TimePicker'
 
 
-const NewScheduleSelection = () => {
+const NewScheduleSelection = () =>
+{
   return (
     <div className='flex flex-col sm:flex-row-reverse justify-center items-center bg-[#E7E7E7]'>
 
-         <div className='flex w-full sm:w-2/12 justify-end'>
-        <QueryPanel/>
-        </div>
+      <div className='flex w-full sm:w-2/12 justify-end'>
+        <QueryPanel />
+      </div>
 
-        <div className='flex flex-col justify-evenly items-center w-full sm:w-10/12 h-screen    '>
-            <DatePicker />
-            <TimePicker/>
-        </div>
+      <div className='flex flex-col justify-evenly items-center w-full sm:w-10/12 h-screen    '>
+        <DatePicker />
+        <TimePicker />
+      </div>
 
 
 
